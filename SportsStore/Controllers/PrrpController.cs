6c8761b65e2a31755cf74a3c9e9bde94bf659e77
wangjiahao <wangjiahao@zjzx.cn>
2017-06-07 17:37:08 +0800

    提交

--- SportsStore/Controllers/PrrpController.cs
+++ SportsStore/Controllers/PrrpController.cs
@@ -1,4 +1,8 @@
-﻿using SportsStore.Models;
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin.Security;
+using SportsStore.Infrastructure.Identity;
+using SportsStore.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -21,13 +25,13 @@ namespace SportsStore.Controllers
         {
             return View(this.repository.Products);
         }
-
+        [Authorize(Roles = "Administrators")]
         public async Task<ActionResult> SaveProduct(Product product)
         {
             await this.repository.SaveProductAsync(product);
             return RedirectToAction("Index");
         }
-
+        [Authorize(Roles = "Administrators")]
         public async Task<ActionResult> DeleteProduct(int id)
         {
             await this.repository.DeleteProductAsync(id);
@@ -38,20 +42,32 @@ namespace SportsStore.Controllers
         {
             return View(this.repository.Orders);
         }
-
+        [Authorize(Roles = "Administrators")]
         public async Task<ActionResult> SaveOrder(Order order)
         {
             await this.repository.SaveOrderAsync(order);
             return RedirectToAction("Orders");
         }
-
+        [Authorize(Roles = "Administrators")]
         public async Task<ActionResult> DeleteOrder(int id)
         {
             await this.repository.DeleteOrderAsync(id);
             return RedirectToAction("Orders");
         }
 
+        public async Task<ActionResult> SignIn()
+        {
+            IAuthenticationManager authMgr = HttpContext.GetOwinContext().Authentication;
+            StoreUserManager userMgr = HttpContext.GetOwinContext().GetUserManager<StoreUserManager>();
+            StoreUser user = await userMgr.FindAsync("Admin", "secret");
+            authMgr.SignIn(await userMgr.CreateIdentityAsync(user, DefaultAuthenticationTypes.ApplicationCookie));
+            return RedirectToAction("Index");
+        }
 
-
+        public ActionResult SignOut()
+        {
+            HttpContext.GetOwinContext().Authentication.SignOut();
+            return RedirectToAction("Index");
+        }
     }
 }
